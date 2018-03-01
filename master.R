e1d846965b0a557b810f55cff858c9b6ac9d6f9b
racdale <racdale@gmail.com>
2018-03-01 13:12:16 -0800

    Added loops with input / without input

--- master.R
+++ master.R
@@ -1,6 +1,8 @@
 setwd('/Users/rickdale/Dropbox/new.projects/multiModal/Bhat and Dale/github/rnndfi')
 source('esn_functions.R')
-input = '123412341234'
+library(ggplot2)
+#input = '123412341234'
+input = '1212121212'
 input = clean_text(input) # do some cleaning
 data = make_text_input(input) # create list of items such as localist representation, char codes
 dataIn = data$data
@@ -9,8 +11,32 @@ dataOut = dataIn[2:nrow(dataIn),] # it's prediction (then generation), so take o
 dataIn = dataIn[1:(nrow(dataIn)-1),] # one less, for prediction... so sizes match
 targets = dataOut # for targets; in prior code, we might want to accumulate nepochs of target, so we separate from dataOut
 
-sz_res = 20
-esn = build_deep_esn(sizes=sz_res,in_size=ncol(dataIn),fac=1,a=.9)
-history = run_deep_esn(esn,iterations=400*nrow(dataIn),inputs=c(),passInputToHistory=F)
-write.table(history,row.names=F,col.names=F,file='data.txt')
-system('/Users/rickdale/anaconda3/bin/python3.6m sindy_in.py > eqns.txt')
+#eqns = readChar('eqns.txt',nchars=file.info('eqns.txt')$size)
+#eqns = unlist(strsplit(eqns,'\n'))
+#eqns[1]
+
+sz_res = 10
+data_res = c()
+for (fac in seq(from=.2,to=2,by=.4)) {
+  print(fac)
+  for (j in 1:5) {
+    esn = build_deep_esn(sizes=sz_res,in_size=ncol(dataIn),fac=fac,a=.9)
+    history = run_deep_esn(esn,iterations=1000*nrow(dataIn),inputs=c(),passInputToHistory=F)
+    history = history[1000:nrow(history),]
+    write.table(history,row.names=F,col.names=F,file='data.txt')
+    system('/Users/rickdale/anaconda3/bin/python3.6m sindy_in.py > eqns.txt')
+    prog_length = file.info('eqns.txt')$size    
+    data_res = rbind(data_res,data.frame(fac=fac,i=j,prog_length=prog_length,type='no-input'))
+    
+    history = run_deep_esn(esn,iterations=1000*nrow(dataIn),inputs=dataIn,passInputToHistory=F)
+    history = history[1000:nrow(history),]
+    write.table(history,row.names=F,col.names=F,file='data.txt')
+    system('/Users/rickdale/anaconda3/bin/python3.6m sindy_in.py > eqns.txt')
+    prog_length = file.info('eqns.txt')$size    
+    data_res = rbind(data_res,data.frame(fac=fac,i=j,prog_length=prog_length,type='with-input'))
+  }
+}
+plot(data_res$fac,data_res$prog_length)
+ggplot(data_res)+aes(x=fac,y=prog_length,col=type)+geom_smooth()
+
+
--- sindy_in.py
+++ sindy_in.py
@@ -13,7 +13,7 @@ n_rows = x.shape[0]/2
 x_train, x_test = x[:int(n_rows/2)], x[int(n_rows/2):]
 
 kw = dict(fit_intercept=True, normalize=False)
-model = SINDy(dt=0.1, degree=2, threshold=0.15, alpha=1.0, n_jobs=-1, kw=kw).fit(x_train)
+model = SINDy(dt=1, degree=1, threshold=0.01, alpha=1.0, n_jobs=-1, kw=kw).fit(x_train)
 # myops = {'sin': np.sin, 'cos': np.cos}
 # model = SINDy(dt=0.1, degree=3, threshold=0.15, operators=myops,
 #               alpha=1.0, n_jobs=-1, kw=kw).fit(x_train)
