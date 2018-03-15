53a6684c16a0c486cf6e3477c3908e74375402a3
racdale <racdale@gmail.com>
2018-03-15 08:48:04 -0700

    adding test of control

--- sindy_in.py
+++ sindy_in.py
@@ -6,17 +6,20 @@ from sparsereg.model import SINDy
 
 x = np.loadtxt("data.txt")
 n_rows = x.shape[0]/2
-
-#xd = x[:,0:3]
-#print(xd.shape)
+n_cols = x.shape[1]
+n_controls = 4
 
 x_train, x_test = x[:int(n_rows/2)], x[int(n_rows/2):]
 
 kw = dict(fit_intercept=True, normalize=False)
-model = SINDy(dt=.1, degree=1, threshold=0.05, alpha=1.0, n_jobs=-1, kw=kw).fit(x_train)
-# myops = {'sin': np.sin, 'cos': np.cos}
-# model = SINDy(dt=0.1, degree=3, threshold=0.15, operators=myops,
-#               alpha=1.0, n_jobs=-1, kw=kw).fit(x_train)
+model = SINDy(dt=1/60, degree=1, threshold=0.1, alpha=1, n_jobs=-1, kw=kw)
+
+y_train = x_train[:,n_controls:n_cols] # create separate y so x contains "control"
+y_test = x_test[:,n_controls:n_cols]
+y_train = model.derivative.transform(y_train)
+y_test = model.derivative.transform(y_test)
+
+model = model.fit(x_train,y=y_train)
 
 print("Score on test data ", model.score(x_test))
 
